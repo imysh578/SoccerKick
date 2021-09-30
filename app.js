004db84278709089ae9fb28238cc8a053866720d
SUN YOUNG GYU <skh01047@naver.com>
2021-09-30 18:46:56 +0900

    푸쉬 제대로 안 돼서 다시 함 ㅠㅠ

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
-const mysql = require('mysql2');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
+const mysql = require("mysql2");
 
-
-const { sequelize } = require('./models');
-const indexRouter = require('./routes');
-const usersRouter = require('./routes/users');
+const { sequelize } = require("./models");
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 const PORT = 3000;
 // 기존 포트 넘버 없으면 3000으로 설정
-app.set('port', process.env.PORT || PORT);
+app.set("port", process.env.PORT || PORT);
 // nunjucks를 기본 엔진으로 설정
-app.set('view engine', 'html');
-nunjucks.configure('views', {
+app.set("view engine", "html");
+nunjucks.configure("views", {
   express: app,
   watch: true,
 });
 
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-// const connection = mysql.createConnection({
-//   host: "jadu.cwmnyyljxyku.ap-northeast-2.rds.amazonaws.com",
-//   user: "jadu",
-//   password: "whwkdtjrgns",
-//   database: "soccer",
-// });
-
-// connection.connect();
-// let user;
-// connection.query("SELECT * FROM user", function (err, results, fields) {
-//     if (err) throw err;
-//     user = results;
-// });
-
-sequelize.sync({force:false})
-.then(()=>{
-  console.log('Database connected successfully');
-})
-.catch((err)=>{
-  console.error(err);
-});
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-// app.use('/user', usersRouter);
+app.use("/", indexRouter);
+app.use("/", usersRouter);
 // app.use('/teams', teamsRouter);
 // app.use('/team_board', teamBoardRouter);
 // app.use('/team_comment', teamCommentRouter);
@@ -60,14 +45,13 @@ app.use('/', indexRouter);
 // app.use('/battle_board', battleBoardRouter);
 // app.use('/battle_comment', battleCommentRouter);
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'soccer' ? err:{};
+  res.locals.error = process.env.NODE_ENV !== "soccer" ? err : {};
   res.status(err.static || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), 'port is ready');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "port is ready");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,10 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 
 const router = express.Router();
-router
-  .route('/')
-  .get(async(req, res, next)=>{
-    try {
-      const users = await User.findAll();
-      res.json(users);
-    } catch (err) {
-      next(err);
-    }
-  })
 
+router.get("/user", (req, res) => {
+  router.render("uset.html");
+});
 
-  module.exports = router;
+module.exports = router;
