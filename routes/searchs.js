a0773a1c5160d86ad2785956b643582bb4420a81
jkdh <jwalk0510@gmail.com>
2021-10-13 17:57:31 +0900

    search css추가, js추가,  3개 검색카테고리

--- routes/searchs.js
+++ routes/searchs.js
@@ -4,11 +4,14 @@ const url = require("url");
 const Sequelize = require("sequelize");
 const Teams = require("../models/teams");
 const Users = require("../models/users");
+const Mercenary_boards = require("../models/mercenary_boards");
+const Battle_boards = require("../models/battle_boards");
 const Op = Sequelize.Op; // LIKE
 
 const router = express.Router();
 
 /* 검색 기능 */
+//---------------------구단리스트
 router.get('/', async (req, res, next) => {
     try {
       let queryData = url.parse(req.url, true).query;   //파싱해주기? 콘솔:[Object: null prototype] { q: '*****' }
@@ -16,7 +19,7 @@ router.get('/', async (req, res, next) => {
       let select = queryData.select;    //?
   
       /* 제목/내용 검색 */
-      const teams = await Teams.findAll({
+      const teamS = await Teams.findAll({
         where: {
         [Op.or]: [{
             team_name : {               // 탐색해볼 테이블들.
@@ -28,7 +31,7 @@ router.get('/', async (req, res, next) => {
             }
           },
             {
-            team_headCount : {
+            team_area : {
               [Op.like]: "%" + q + "%"
             }
           },
@@ -36,7 +39,52 @@ router.get('/', async (req, res, next) => {
         },
         order: [['team_name', 'DESC']],
       });
-      console.log(teams)
+      console.log(teamS)
+      //---------------------용병
+      const merceS = await Mercenary_boards.findAll({
+        where: {
+        [Op.or]: [{
+            mercenary_board_title : {               // 탐색해볼 테이블들.
+              [Op.like]: "%" + q + "%"  //내가입력한 값을 유사검색하는 op.like
+            }
+          }, {
+            mercenary_select : {
+              [Op.like]: "%" + q + "%"
+            }
+          },
+            {
+            user_area : {
+              [Op.like]: "%" + q + "%"
+            }
+          },
+        ]
+        },
+        order: [['mercenary_select', 'DESC']],
+      });
+      console.log(merceS)
+
+      //---------------------배틀
+      const battleS = await Battle_boards.findAll({
+        where: {
+        [Op.or]: [{
+            battle_board_teamName : {               // 탐색해볼 테이블들.
+              [Op.like]: "%" + q + "%"  //내가입력한 값을 유사검색하는 op.like
+            }
+          }, {
+            battle_board_area : {
+              [Op.like]: "%" + q + "%"
+            }
+          },
+            {
+            battle_board_title : {
+              [Op.like]: "%" + q + "%"
+            }
+          },
+        ]
+        },
+        order: [['battle_board_title', 'DESC']],
+      });
+      console.log(battleS)
   
     //   /* 내용 검색 */
     //   const contents = await Teams.findAll({
@@ -91,7 +139,11 @@ router.get('/', async (req, res, next) => {
     //   }
   
       res.render('search_result', {
-       teams: teams,
+       teams: teamS,
+       merces: merceS,
+       battles: battleS
+       
+
       });
     } catch (error) {
       console.error(error);
