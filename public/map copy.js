bb739bf0aa7f02e0424caed83eff131d6db734a9
dongnycklee <jwalk0510@gmail.com>
2021-10-14 16:23:48 +0900

    맵건드는중..

--- public/map copy.js
+++ public/map copy.js
@@ -13,7 +13,24 @@ var positions = [
     {
         title: "광나루한강공원축구장",
         latlng: new kakao.maps.LatLng(37.55242480860552, 127.1221643613391),
-        content: '서울특별시 강동구 암사동 광나루한강공원',
+        content:'ㅇㅇ<div class="wrap">' + 
+        '    <div class="info">' + 
+        '        <div class="title">' + 
+        '            카카오 스페이스닷원' + 
+        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' + 
+        '        </div>' + 
+        '        <div class="body">' + 
+        '            <div class="img">' +
+        '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+        '           </div>' + 
+        '            <div class="desc">' + 
+        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' + 
+        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' + 
+        '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' + 
+        '            </div>' + 
+        '        </div>' + 
+        '    </div>' +    
+        '</div>'    
     },
     {
         title: "동명축구연습장",
